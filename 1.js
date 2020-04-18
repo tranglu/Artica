c926604054d080bcf6d4c91dfb965fc510d499bb
tranglu <lu.t.t.trang@gmail.com>
2020-04-18 17:11:14 +0700

    wow.js

--- 1.js
+++ 1.js
@@ -1,4 +1,16 @@
 ﻿ $(function(){
  
+	// xu ly phan responsive bang jquery . 
+	// tinh ra so div ten .motmon
+
+	//soluongmon = $('.motmon').length ;
+	//docao = soluongmon* 120; 
+	//console.log(docao );
+
+	//$('.noidungct').css({'height':docao})
+
+	// xem div .motmon co ton tai hay khong 
+	 
+	 new WOW().init();
 })  
  
